4fba9d933d376c903dbce5fd7bf88b5fae2f6314
Your Name <you@example.com>
2022-01-19 13:00:55 +0800

    docs(): add keyword and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,22 +17,22 @@ function HomepageHeader() {
           <Link
             className={`button button--secondary button--lg ${styles.linkMargin}`}
             to="/docs/">
-              阅读文档
+            阅读文档
           </Link>
           <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started/tutorial">
-              开始教程
+            开始教程
           </Link>
-          </div>
+        </div>
 
-          <div className={styles.sandbox}>
-            <iframe src="https://codesandbox.io/embed/cool-villani-b80ts?fontsize=14&hidenavigation=1&theme=dark"
+        <div className={styles.sandbox}>
+          <iframe src="https://codesandbox.io/embed/cool-villani-b80ts?fontsize=14&hidenavigation=1&theme=dark"
             style={{ width: "100%", height: 500, border: 0, borderRadius: 4, overflow: "hidden" }}
             title="cool-villani-b80ts"
-              allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-              sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-            ></iframe>
+            allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+          ></iframe>
         </div>
       </div>
     </header>
@@ -43,9 +43,9 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      image={'https://fanyi.baidu.com/appdownload/download.html?tab=app&appchannel=webbannerfinal&fr=pcproduct'}
-      description="React Router v6 中文">
+      title={`React路由`}
+      image={'https://www.reactrouter.cn/img/logo.png'}
+      description="React路由,React路由第六版,React路由第6版,React路由中文,React Router,react-router,react-router-dom,react-router v6">
       <HomepageHeader />
       <main>
       </main>
